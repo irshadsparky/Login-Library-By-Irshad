1b8297b36d8a71ca028a89de45cca93e46dfffdd
irshadsparky <ir9977@gmail.com>
2018-02-09 18:43:17 +0530

    Update ApplicationUtils.java

--- loginlibraryirshad/src/main/java/irshad/sheikh/loginlibrary/util/ApplicationUtils.java
+++ loginlibraryirshad/src/main/java/irshad/sheikh/loginlibrary/util/ApplicationUtils.java
@@ -25,6 +25,7 @@ public class ApplicationUtils {
     public static <T> void addToRequestQueue(Request<T> req, String tag) {
         // set the default tag if tag is empty
         req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+         req.setShouldCache(false);
         getRequestQueue().add(req);
     }
 
